9dbe5e3f5c8bdebceaf235fc5ff72bb0921418e3
이지민 <leechimins@naver.com>
2024-02-24 12:07:58 +0900

    home.vue에 주석 한줄 추가

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -9,6 +9,7 @@
       <router-link to="/signup">회원가입</router-link> |  <!-- SignUp.vue로 연결되는 라우터 -->
       <router-link to="/login">로그인</router-link> |  <!-- Login.vue로 연결되는 라우터 -->
       <router-link to="/questionlist">게시판</router-link>  <!-- SignUp.vue로 연결되는 라우터 -->
+      <!-- 주석 -->
     </div>
     <img alt="Vue logo" src="../assets/logo.png">
   </div>
