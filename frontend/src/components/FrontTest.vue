a65ae2a6c31a9afb931086fda13ecbd29e2a4781
hyojean <hrn0314@naver.com>
2024-03-09 11:40:36 +0900

    프론트 수정

---
+++ frontend/src/components/FrontTest.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+      <input type="text" v-model="b1" name="b1" placeholder="1 or 2">
+      <button @click="button">Click</button>
+      <p>{{a}}</p>
+      <button @click="postBtn">PostClick</button>
+      <p>{{c}}</p>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: 'FrontTest',
+  data: () => ({
+    b1: '',
+    a: '',
+    c: ''
+  }),
+  methods: {
+    button () {
+      this.axios.get('/api/example', {params: {tEst:this.b1} }).then(result => {
+        console.log(result.data)
+        this.a = result.data
+      })
+    },
+    postBtn () {
+      const fd = new FormData()
+      fd.append('demo', this.b1)
+      this.axios.post('/api/example2', fd).then(result => {
+        this.c = result.data
+      })
+    }
+  }
+}
+</script>
--- src/main/java/com/mysite/basketproject/BasketprojectApplication.java
+++ src/main/java/com/mysite/basketproject/BasketprojectApplication.java
@@ -2,8 +2,9 @@ package com.mysite.basketproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class BasketprojectApplication {
 
     public static void main(String[] args) {
